936e573d7bbeedf06a8549701bdf68be98f4d20c
andreasduerloo <andreasduerloo@gmail.com>
2022-06-15 18:51:04 +0200

    Loop through arguments with while let

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = env::args(); // Type is env::Args
+
+    match arguments.len() {
+        0 | 1 => {
+            println!("No arguments were passed. Exiting.");
+            return;
+        },
+        _ => {
+            // Skip the first argument
+            let mut hash = arguments.next(); // Type is Option(String)
+
+            while let Some(content) = hash {
+                println!("Argument: {}", content);
+                hash = arguments.next();
+            }
+        }
+    }
 }
