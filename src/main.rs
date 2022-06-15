37d9630f377236408e3a88dde420b99725c9a7af
andreasduerloo <andreasduerloo@gmail.com>
2022-06-15 20:29:06 +0200

    Formatted hex with lowercase letters

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use gleephs;
 
 fn main() {
-    let mut arguments = env::args(); // Type is env::Args
+    let mut arguments = env::args();
 
     match arguments.len() {
         0 | 1 => {
@@ -10,14 +10,14 @@ fn main() {
             return;
         },
         _ => {
-            let mut hash = arguments.nth(1); // Type is Option(String) - skip the first argument
+            let mut hash = arguments.nth(1); // Skip the first argument
 
             while let Some(content) = hash {
                 if gleephs::input::is_hex(&content) {
                     let glyph_input = gleephs::input::squash(&content);
                     
                     if let Ok(number) = glyph_input {
-                        println!("Raw input: {:X}", number);
+                        println!("Raw input: {:x}", number);
                     }
                 }
 
